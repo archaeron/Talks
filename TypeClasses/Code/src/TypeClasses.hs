701adee67df5e1c0d5b920d29bb99d07d2081da0
Nicolas Gagliani <whatamaps@gmail.com>
2015-02-01 22:32:20 +0100

    added the Show and Applicative typeclass

--- TypeClasses/Code/src/TypeClasses.hs
+++ TypeClasses/Code/src/TypeClasses.hs
@@ -1,14 +1,25 @@
 module TypeClasses where
 
+import Control.Applicative
+
 data Option a =
-  Some a | None
-  deriving (Show)
+    Some a | None
+    deriving (Show)
+
+instance (Show a) => Show (Option a) where
+    show (Some a) = "Some " ++ show a
+    show None = "None"
 
 instance (Eq a) => Eq (Option a) where
-  (Some x) == (Some y) = x == y
-  None == None = True
-  _ == _ = False
+    (Some x) == (Some y) = x == y
+    None == None = True
+    _ == _ = False
 
 instance Functor Option where
-  fmap f (Some a) = Some (f a)
-  fmap f None = None
+    fmap f (Some a) = Some (f a)
+    fmap f None = None
+
+instance Applicative Option where
+    pure a = Some a
+    (Some f) <*> (Some a) = Some $ f a
+    _ <*> _ = None
